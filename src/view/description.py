d124dcd644b0d544a1a9e7ea261b7f1254d76254
Gustavo Millen <millengustavo@gmail.com>
2020-06-05 09:11:15 -0300

    Fix typo

--- src/view/description.py
+++ src/view/description.py
@@ -4,8 +4,8 @@ import streamlit as st
 def print_description() -> None:
     st.title("Salários em carreiras de dados")
     st.markdown("""
-    *Quanto estão ganhando as pessoas com características parecidas com a sua?*
-    
+    *Quanto estão ganhando as pessoas com características parecidas com as suas?*
+
     Fonte dos dados: Pesquisa de mercado de Data Science feita pelo Data Hackers
     [https://www.kaggle.com/datahackers/pesquisa-data-hackers-2019](https://www.kaggle.com/datahackers/pesquisa-data-hackers-2019)
     """)
