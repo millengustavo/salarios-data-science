16893b85fe9a71e78c4ca6de59b092aabfdb8970
Gustavo Millen <millengustavo@gmail.com>
2020-11-05 17:37:34 -0300

    Add new layout organization with columns

--- src/data_science_salary.py
+++ src/data_science_salary.py
@@ -9,6 +9,7 @@ from view.salary import plot_salary, print_salary, plot_segmented_salary
 
 from controller.menu import filter_data
 
+st.set_page_config(layout="wide")
 
 df = load_data()
 
@@ -16,12 +17,17 @@ print_description()
 
 df = filter_data(df)
 
-plot_salary(df)
 print_salary(df)
+plot_salary(df)
 
-plot_segmented_salary(df, "gender", "gênero")
-plot_segmented_salary(df, "age", "idade")
-plot_segmented_salary(df, "living_state", "estado de residência")
-plot_segmented_salary(df, "manager", "tem cargo gerencial?")
-plot_segmented_salary(df, "is_data_science_professional", "é Data Scientist?")
-plot_segmented_salary(df, "online_courses", "fez cursos online?")
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    plot_segmented_salary(df, "gender", "gênero")
+    plot_segmented_salary(df, "living_state", "estado de residência")
+    plot_segmented_salary(df, "is_data_science_professional", "é Data Scientist?")
+
+with col2:
+    plot_segmented_salary(df, "age", "idade")
+    plot_segmented_salary(df, "manager", "tem cargo gerencial?")
+    plot_segmented_salary(df, "online_courses", "fez cursos online?")
